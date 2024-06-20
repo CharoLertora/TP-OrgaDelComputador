94311cedb2fe4396369601e155300629921b49bb
tomasghiglione <tghiglione@fi.uba.ar>
2024-06-20 15:13:06 -0300

    fin de juego en cualquier momento

--- inicio.asm
+++ inicio.asm
@@ -25,24 +25,24 @@ section .data
                 db  -1, -1,  2,  2,  2, -1, -1
                 db  -1, -1,  2,  2,  2, -1, -1
 
-    salto_linea         db 10, 0        
-    simbolo_fuera       db ".", 0
-    simbolo_oca         db 'O', 0
-    simbolo_zorro       db 'X', 0
-    simbolo_vacio       db ' ', 0
-    simbolo_separador   db '|', 0
-    longfila            db 7
+    salto_linea                 db 10, 0        
+    simbolo_fuera_tablero       db ".", 0
+    simbolo_oca                 db 'O', 0
+    simbolo_zorro               db 'X', 0
+    simbolo_espacio_vacio       db ' ', 0
+    simbolo_separador           db '|', 0
+    ;longfila                    db 7     ME TIRA ERROR USANDO VARIABLE, HARDCODEO EL 7 POR EL MOMENTO
 
-    mensaje_mover_oca                   db "Ingrese la fila y columna de la oca a mover (ejemplo: 3 3): ", 0
-    mensaje_mover_oca_direccion         db "Mueva la oca con w: arriba /a: izquierda /s: abajo /d: derecha ", 0
-    mensaje_movimiento_invalido_oca     db "Movimiento invalido para la oca, intente nuevamente", 0
+    mensaje_mover_oca                   db "Ingrese la fila y columna de la oca a mover (ejemplo: 3 3). Presione f para salir de la partida: ", 0
+    mensaje_mover_oca_direccion         db "Mueva la oca con w: arriba /a: izquierda /s: abajo /d: derecha. Presione f para salir de la partida: ", 0
     formatInputFilCol                   db "%hhu %hhu", 0                               ; Formato para leer enteros de 1 byte
     msjErrorInput                       db "Los datos ingresados son inválidos. Intente nuevamente.", 0
-    mensaje_mover_zorro                 db "Mueva el zorro con w: arriba /a: izquierda /s: abajo /d: derecha /e: arriba-derecha /q: arriba-izquierda /z: abajo-izquierda /x: abajo-derecha ", 0
+    mensaje_mover_zorro                 db "Mueva el zorro con w: arriba /a: izquierda /s: abajo /d: derecha /e: arriba-derecha /q: arriba-izquierda /z: abajo-izquierda /x: abajo-derecha. Presione f para salir de la partida: ", 0
     mensaje_mov_invalido                db "Movimiento invalido, intente nuevamente", 0
     mensaje_ingresar_j1                 db "Ingrese el nombre del jugador 1 (zorro): ", 0
     mensaje_ingresar_j2                 db "Ingrese el nombre del jugador 2 (ocas): ", 0
     mensaje_ganador                     db "El ganador es: ", 0
+    mensaje_fin_juego                   db "El juego ha sido abandonado.", 0
     
 section .bss
     buffer          resb 350  ; Suficiente espacio para el tablero con saltos de línea
@@ -52,156 +52,160 @@ section .bss
     inputValido     resb 1
     posicion_oca    resq 1
     input_zorro     resb 10
-    nombre_j1       resb 50
-    nombre_j2       resb 50
+    nombre_jugador1 resb 50
+    nombre_jugador2 resb 50
     turno           resb 1
 
 section .text
 main:
     sub     rsp,8
-    call    ingresar_nombres        ;llamo a la subrutina para ingresar nombres
+    call    ingresar_nombres_jugadores        ;llamo a la subrutina para ingresar nombres
     add     rsp,8
     sub     rsp,8
-    call    construir_tablero
+    call    construir_tablero       ;llamo a la subrutina para construir el tablero inicial
     add     rsp,8
     sub     rsp,8
-    call    imprimir_tablero
+    call    imprimir_tablero        ;llamo a la subrutina para imprimir el tablero
     add     rsp,8
 
 loop_juego:
     mov     al, [turno]     ; veo de quien es el turno
     cmp     al, 1
-    je turno_zorro
+    je turno_zorro          ; si es el turno del zorro, voy a la etiqueta turno_zorro
     cmp     al, 2
-    je turno_ocas
+    je turno_ocas           ; si es el turno de las ocas, voy a la etiqueta turno_ocas
 
 turno_zorro:
     sub     rsp,8
-    call pedir_movimiento_zorro     
+    call    pedir_movimiento_zorro  ;llamo a la subrutina para pedir movimiento del zorro
     add     rsp,8
+    cmp     byte [input_zorro], 'f' ; Verificar si se desea abandonar la partida
+    je      fin_juego
     sub     rsp,8
-    call mover_zorro
+    call    mover_zorro              ;llamo a la subrutina para mover al zorro
     add     rsp,8
-    cmp     byte [inputValido], 'R'     
-    je turno_zorro
-    mov     byte [turno], 2
-    jmp continuar_juego
+    cmp     byte [inputValido], 'R'  ;comparo si el movimiento del zorro fue inválido
+    je      turno_zorro              ;si fue inválido, vuelvo a pedir movimiento del zorro
+    mov     byte [turno], 2          ;si fue válido, cambio el turno a las ocas
+    jmp     continuar_juego          ;voy a la etiqueta continuar_juego
 
 turno_ocas:
     sub     rsp,8
-    call pedir_movimiento_oca
+    call    pedir_movimiento_oca     ;llamo a la subrutina para pedir movimiento de la oca
     add     rsp,8
+    cmp     byte [input_oca], 'f'    ; Verificar si se desea abandonar la partida
+    je      fin_juego
     sub     rsp,8
-    call mover_oca
+    call    mover_oca                ;llamo a la subrutina para mover la oca
     add     rsp,8
-    cmp byte [inputValido], 'R'
-    je turno_ocas
-    mov byte [turno], 1
+    cmp     byte [inputValido], 'R'  ;comparo si el movimiento de la oca fue inválido
+    je      turno_ocas               ;si fue inválido, vuelvo a pedir movimiento de la oca
+    mov     byte [turno], 1          ;si fue válido, cambio el turno al zorro
 
 continuar_juego:
     sub     rsp,8
-    call construir_tablero
+    call    construir_tablero       ;reconstruyo el tablero después de cada turno
     add     rsp,8
     sub     rsp,8
-    call imprimir_tablero
+    call    imprimir_tablero        ;imprimo el tablero después de cada turno
     add     rsp,8
-    jmp loop_juego
+    jmp     loop_juego              ;vuelvo al inicio del bucle del juego
 
     ret
 
-ingresar_nombres:
+ingresar_nombres_jugadores:
     mov     rdi, mensaje_ingresar_j1   
     mPuts
-    mov     rdi, nombre_j1              ; guardo el nombre de cada jugador
+    mov     rdi, nombre_jugador1              ; guardo el nombre de cada jugador
     mGets
     mov rdi, mensaje_ingresar_j2
     mPuts
-    mov rdi, nombre_j2
+    mov rdi, nombre_jugador2
     mGets
 
     mov byte [turno], 1  ; Comienza el turno del zorro
     ret
 
 construir_tablero:
-    mov rbx, 1  ; i que será la fila, iniciada en 1 y no aumenta hasta no terminar las 7 columnas
-    mov r10, 1  ; j que será la columna
-    mov rdi, buffer  ; Apuntar al inicio del buffer
+    mov     rbx, 1            ; i que será la fila, iniciada en 1 y no aumenta hasta no terminar las 7 columnas
+    mov     r10, 1            ; j que será la columna
+    mov     rdi, buffer       ; Apuntar al inicio del buffer
 
-imprimir_siguiente:   
-    mov rax, rbx
-    dec rax
-    imul rax, rax, 7   ; (i-1) * longfila
-    mov rdx, r10
-    dec rdx
-    add rax, rdx       ; (i-1) * longfila + (j-1)
-    mov rsi, tablero
-    add rsi, rax       ; rsi apunta a la posición actual en el tablero
+imprimir_siguiente_caracter:   
+    mov     rax, rbx
+    dec     rax
+    imul    rax, rax, 7       ; (i-1) * longfila
+    mov     rdx, r10
+    dec     rdx
+    add     rax, rdx          ; (i-1) * longfila + (j-1)
+    mov     rsi, tablero
+    add     rsi, rax          ; rsi apunta a la posición actual en el tablero
 
-    cmp byte [rsi], -1      ;segun el numero en tablero imprimo un caracter distinto
-    je  imprimir_fuera                
-    cmp byte [rsi], 2           
-    je  imprimir_vacio              
-    cmp byte [rsi], 1         
-    je  imprimir_oca
-    cmp byte [rsi], 3
-    je  imprimir_zorro
+    cmp     byte [rsi], -1      ;segun el numero en tablero imprimo un caracter distinto
+    je      imprimir_fuera_tablero                
+    cmp     byte [rsi], 2           
+    je      imprimir_espacio_vacio              
+    cmp     byte [rsi], 1         
+    je      imprimir_oca
+    cmp     byte [rsi], 3
+    je      imprimir_zorro
 
-imprimir_fuera:
-    mov al, [simbolo_separador]
+imprimir_fuera_tablero:
+    mov     al, [simbolo_separador]
     stosb
-    mov al, [simbolo_fuera]
+    mov     al, [simbolo_fuera_tablero]
     stosb
-    mov al, [simbolo_separador]
+    mov     al, [simbolo_separador]
     stosb
-    jmp continuar_construyendo
+    jmp     continuar_construyendo_tablero
 
 imprimir_oca:
-    mov al, [simbolo_separador]
+    mov     al, [simbolo_separador]
     stosb
-    mov al, [simbolo_oca]
+    mov     al, [simbolo_oca]
     stosb
-    mov al, [simbolo_separador]
+    mov     al, [simbolo_separador]
     stosb
-    jmp continuar_construyendo
+    jmp     continuar_construyendo_tablero
 
 imprimir_zorro:
-    mov al, [simbolo_separador]
+    mov     al, [simbolo_separador]
     stosb
-    mov al, [simbolo_zorro]
+    mov     al, [simbolo_zorro]
     stosb
-    mov al, [simbolo_separador]
+    mov     al, [simbolo_separador]
     stosb
-    jmp continuar_construyendo
+    jmp     continuar_construyendo_tablero
 
-imprimir_vacio:
-    mov al, [simbolo_separador]
+imprimir_espacio_vacio:
+    mov     al, [simbolo_separador]
     stosb
-    mov al, [simbolo_vacio]
+    mov     al, [simbolo_espacio_vacio]
     stosb
-    mov al, [simbolo_separador]
+    mov     al, [simbolo_separador]
     stosb
-    jmp continuar_construyendo
+    jmp     continuar_construyendo_tablero
 
-continuar_construyendo:
-    inc r10 ; Incrementar en uno para tener la siguiente columna
-    cmp r10, 8    ; Si no llegué a la columna 7, construyo el siguiente elemento de la misma fila              
-    jl imprimir_siguiente       
+continuar_construyendo_tablero:
+    inc     r10                ; Incrementar en uno para tener la siguiente columna
+    cmp     r10, 8             ; Si no llegué a la columna 7, construyo el siguiente elemento de la misma fila              
+    jl      imprimir_siguiente_caracter       
 
     ; Añadir un salto de línea al final de la fila
-    mov al, [salto_linea]
+    mov     al, [salto_linea]
     stosb
-    mov r10, 1
-    inc rbx     ; Incremento en uno la fila (siguiente fila)
-    cmp rbx, 8  ; Si llegué a la fila 7, termino la construcción
-    je fin_construir
+    mov     r10, 1
+    inc     rbx                ; Incremento en uno la fila (siguiente fila)
+    cmp     rbx, 8             ; Si llegué a la fila 7, termino la construcción
+    je      fin_construir_tablero
 
-    jmp imprimir_siguiente
+    jmp     imprimir_siguiente_caracter
 
-fin_construir:
+fin_construir_tablero:
     ret
 
 imprimir_tablero:
-    mov rdi, buffer
+    mov     rdi, buffer
     mPuts
     ret
 
@@ -219,63 +223,63 @@ mover_zorro:
 buscar_zorro:
     lodsb
     cmp al, 3
-    je encontrado_zorro
+    je zorro_encontrado
     loop buscar_zorro
     ret
 
-encontrado_zorro:
+zorro_encontrado:
     mov rbx, rsi   ; Mueve el valor del registro rsi (posición actual del zorro) a rbx
     dec rbx         ; Decrementa rbx en 1 para apuntar correctamente a la posición actual del zorro
 
     mov rdi, input_zorro
     mov al, [rdi]
     cmp al, 'w'
-    je mover_arriba
+    je mover_zorro_arriba
     cmp al, 's'
-    je mover_abajo
+    je mover_zorro_abajo
     cmp al, 'a'
-    je mover_izquierda
+    je mover_zorro_izquierda
     cmp al, 'd'
-    je mover_derecha
+    je mover_zorro_derecha
     cmp al, 'e'
-    je mover_arriba_derecha
+    je mover_zorro_arriba_derecha
     cmp al, 'q'
-    je mover_arriba_izquierda
+    je mover_zorro_arriba_izquierda
     cmp al, 'z'
-    je mover_abajo_izquierda
+    je mover_zorro_abajo_izquierda
     cmp al, 'x'
-    je mover_abajo_derecha
+    je mover_zorro_abajo_derecha
     ret
 
-mover_arriba:
+mover_zorro_arriba:
     sub rbx, 7                  ; resto 7 a rbx para mover al zorro una fila hacia arriba
     jmp validar_movimiento_zorro 
 
-mover_abajo:
+mover_zorro_abajo:
     add rbx, 7                  ; sumo 7 a rbx para mover al zorro una fila hacia abajo
     jmp validar_movimiento_zorro
 
-mover_izquierda:
+mover_zorro_izquierda:
     dec rbx                     ; resto 1 a rbx para mover al zorro una columna a la izquierda
     jmp validar_movimiento_zorro
 
-mover_derecha:
+mover_zorro_derecha:
     inc rbx                     ; sumo 1 a rbx para mover al zorro una columna a la derecha
     jmp validar_movimiento_zorro
 
-mover_arriba_derecha:
+mover_zorro_arriba_derecha:
     sub rbx, 6                  ; resto 6 a rbx para mover al zorro en diagonal arriba derecha
     jmp validar_movimiento_zorro
 
-mover_arriba_izquierda:
+mover_zorro_arriba_izquierda:
     sub rbx, 8                  ; resto 8 a rbx para mover al zorro en diagonal arriba izquierda
     jmp validar_movimiento_zorro
 
-mover_abajo_izquierda:
+mover_zorro_abajo_izquierda:
     add rbx, 6                  ; sumo 6 a rbx para mover al zorro en diagonal abajo izquierda
     jmp validar_movimiento_zorro
 
-mover_abajo_derecha:
+mover_zorro_abajo_derecha:
     add rbx, 8                   ; sumo 8 a rbx para mover al zorro en diagonal abajo derecha
     jmp validar_movimiento_zorro
 
@@ -298,19 +302,20 @@ pedir_movimiento_oca:
     mPuts
     mov rdi, input_oca
     mGets
-
+    cmp byte [input_oca], 'f'    ; Verificar si se desea abandonar la partida
+    je fin_juego
     ; Validar las coordenadas de la oca
-    sub     rsp,8
+    sub rsp,8
     call validar_coordenadas_oca
-    add     rsp,8
+    add rsp,8
     cmp byte [inputValido], 'S'
     je pedir_direccion_oca
 
     mov rdi, msjErrorInput
     mPuts
-    sub     rsp,8
+    sub rsp,8
     call pedir_movimiento_oca
-    add     rsp,8
+    add rsp,8
     ret
 
 pedir_direccion_oca:
@@ -318,7 +323,6 @@ pedir_direccion_oca:
     mPuts
     mov rdi, input_oca
     mGets
-
     ret
 
 mover_oca:
@@ -366,7 +370,7 @@ validar_movimiento_oca:
 
 movimiento_invalido_oca:
     mov byte [inputValido], 'R'
-    mov rdi, mensaje_movimiento_invalido_oca
+    mov rdi, mensaje_mov_invalido
     mPuts
     ret
 
@@ -413,5 +417,10 @@ validar_coordenadas_oca:
 
 coordenadas_invalidas:
     mov rdi, msjErrorInput
+    mPuts
+    ret
+
+fin_juego:
+    mov     rdi, mensaje_fin_juego  ; Imprimir el mensaje de fin del juego
     mPuts
     ret
